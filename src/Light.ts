a38cf21391dae8e28bef9725c4e5f8b760e5020d
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-20 12:44:55 +0700

    feat: light sprite moves with mouse

---
+++ src/Light.ts
@@ -0,0 +1,22 @@
+﻿import {Scene} from "phaser"
+
+class Light extends Phaser.GameObjects.Sprite
+{
+    constructor(scene: Scene) {
+        super(scene, 0, 0, 'light')
+        scene.add.existing(this)
+    }
+
+    update(time: number, delta: number): void {
+        this.depth = 100
+    }
+
+    public moveToCursor(pointer: Phaser.Input.Pointer): void {
+        console.log("a")
+        this.setX(pointer.x)
+        this.setY(pointer.y)
+        this.scale = 0.1
+    }
+}
+
+export default Light
--- src/game.ts
+++ src/game.ts
@@ -1,1 +1,63 @@
+import * as Phaser from "phaser"
+import Light from "./Light"
 
+class Game extends Phaser.Scene
+{
+    private readonly CELL_SIZE: number = 100
+    private cat: Phaser.GameObjects.Sprite
+    private light: Light
+
+    preload(): void {
+        this.load.image('cat', 'assets/cat.jpg')
+        this.load.image('light', 'assets/light.png')
+    }
+
+    create(): void {
+        this.cat = this.add.sprite(0, 0, 'cat')
+
+        this.cat.setOrigin(0, 0)
+        this.add.text(50, 50, "AAAAA")
+
+        const overlay = this.add.graphics()
+
+        overlay.fillStyle(0x000000, 0.8).fillRect(0, 0, 800, 600)
+
+        const maskGraphics = this.make.graphics()
+
+        maskGraphics.fillStyle(0xffffff)
+        maskGraphics.fillRect(100, 100, 256, 256)
+
+        const mask = new Phaser.Display.Masks.BitmapMask(this, maskGraphics)
+
+        mask.invertAlpha = true
+
+        overlay.setMask(mask)
+
+        this.light = new Light(this)
+
+        this.input.on('pointermove', this.light.moveToCursor, this.light)
+    }
+    
+    update(time: number, delta: number) {
+        this.light.update(time, delta)
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 500,
+    height: 600,
+    fps: {target: 60},
+    backgroundColor: "eeeeee",
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 800},
+            enableBody: true,
+
+        }
+    },
+    scene: [Game]
+}
+
+const game = new Phaser.Game(config)
