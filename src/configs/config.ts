77055b044764b178eb0b496fd8c11eb11d178679
Chuan <chuan.nguyenvn@hcmut.edu.vn>
2023-06-21 09:57:15 +0700

    feat: basic directory structure

---
+++ src/configs/config.ts
@@ -0,0 +1,12 @@
+﻿import Phaser from "phaser";
+
+const GAME_CONFIG: Phaser.Types.Core.GameConfig = {
+    title: 'Project Museum',
+    width: 960,
+    height: 540,
+    parent: 'game',
+    scene: [],
+    backgroundColor: 0x111111,
+};
+
+export default GAME_CONFIG
---
+++ src/configs/constants.ts
@@ -0,0 +1,5 @@
+﻿const CONST = {
+    
+}
+
+export default CONST
